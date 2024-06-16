53040c5f974c85665a30e2dabdda910aa38b1e9a
dipendra <dipendrajr999@gmail.com>
2024-06-16 11:40:09 +0545

    new commit

---
+++ app/src/main/java/com/example/my_mvvm_task/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.my_mvvm_task
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.my_mvvm_task.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent= Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
